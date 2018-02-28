d1fa72d1427fb017cd62af54526f54c0d650cc74
guanqingqi <cyanqi.guan@gmail.com>
2018-02-28 11:16:46 +0800

    add comment

--- src/main/java/top/dolphin/constant/Constants.java
+++ src/main/java/top/dolphin/constant/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     // web root directory
     public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator  + "webapp";
 
-    //web listen port import
+    //web listen port very import
     public static final Integer WEB_PORT = 8080;
 
 }
